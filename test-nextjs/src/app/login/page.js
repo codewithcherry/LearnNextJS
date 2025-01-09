bed3956c14c495c35fb6bcf8398641cee6a549e7
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-09 14:43:53 +0530

    create login and regiter pages

---
+++ test-nextjs/src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-black font-bold text-3xl text-center'> Login page</h1>
+    </div>
+  )
+}
+
+export default Page
---
+++ test-nextjs/src/app/register/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+       <h1 className="text-black text-3xl font-bold m-5 p-5 text-center">Register Page</h1>
+    </div>
+  )
+}
+
+export default Register
