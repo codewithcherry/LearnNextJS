c6edf6493e55a5e22a0ba5305b4b857b6bd0f79b
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-09 15:39:14 +0530

    create nested bynamic page for products and product details based on dynamic product id

---
+++ test-nextjs/src/app/products/[productId]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductView = () => {
+  return (
+    <div >
+      <h1 className='text-2xl text-center font-bold'>product details page on dynamic page id route
+      </h1>
+    </div>
+  )
+}
+
+export default ProductView
---
+++ test-nextjs/src/app/products/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className='text-xl text-center font-semibold m-4 p-5'>
+      <ul>ALL Products
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+        <li>Product 5</li>
+        <li>Product 6</li>
+      </ul>      
+    </div>
+  )
+}
+
+export default Products
